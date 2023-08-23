bbc5c99abd12dc440325d4ccfd283c152ba9be3c
Besl_ol83 <Besl_ol83@student.itstep.org>
2023-08-23 13:55:23 +0300

    Class User + LomBook

--- Hello_java/hello_Java/src/main/java/org/itstep/Main.java
+++ Hello_java/hello_Java/src/main/java/org/itstep/Main.java
@@ -1,7 +1,16 @@
 package org.itstep;
 
+import org.itstep.entities.User;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        User u = new User();
+        u.setEmail("adfadfa");
+        u.setPassword("sdgsrg");
+
     }
+
+
+
 }
---
+++ Hello_java/hello_Java/src/main/java/org/itstep/entities/User.java
@@ -0,0 +1,11 @@
+package org.itstep.entities;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String email;
+    private String password;
+
+}
